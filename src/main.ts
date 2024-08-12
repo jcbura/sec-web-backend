8f53acd66bba37cd42586724085c27576aa7f806
jcbura <jasoncbura@gmail.com>
2024-08-12 15:37:07 -0500

    add port env variable

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  // await app.listen(Number(process.env.PORT)); // FOR LOCAL
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
