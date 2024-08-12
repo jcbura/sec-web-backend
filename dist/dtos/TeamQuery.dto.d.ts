2ef6f9ac83b68b02f7cbe20c1fb450a5ed88071d
jcbura <jasoncbura@gmail.com>
2024-08-12 15:06:18 -0500

    stop tracking dist directory, trying to deploy on something other than vercel

--- dist/dtos/TeamQuery.dto.d.ts
+++
@@ -1,9 +0,0 @@
-declare enum TeamQueryEnum {
-    ALPHABETICAL = "alpha",
-    RANK = "rank",
-    RECORD = "record"
-}
-export declare class TeamQuery {
-    sort: TeamQueryEnum;
-}
-export {};
--- dist/games/controllers/games/games.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { GamesService } from 'src/games/services/games/games.service';
-import { TeamParams } from 'src/dtos/TeamParams.dto';
-export declare class GamesController {
-    private readonly gameService;
-    constructor(gameService: GamesService);
-    getGames(): Promise<any>;
-    getSECCGame(): Promise<any>;
-    getNextGame(params: TeamParams): Promise<any>;
-}
