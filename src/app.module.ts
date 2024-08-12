90d1baa6e569ffd82526bc5cf2872f47331145a0
jcbura <jasoncbura@gmail.com>
2024-08-12 13:11:58 -0500

    hopefully will fix deploying to vercel, first commit after using non-local database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Game } from './typeorm/entities/Game';
 import { Team } from './typeorm/entities/Team';
 import { GamesModule } from './games/games.module';
@@ -11,15 +11,19 @@ import { TeamsModule } from './teams/teams.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: process.env.USERNAME,
-      password: process.env.PASSWORD,
-      database: process.env.DATABASE,
-      entities: [Game, Team],
-      synchronize: false,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [Game, Team],
+        synchronize: false,
+      }),
+      inject: [ConfigService],
     }),
     GamesModule,
     TeamsModule,
